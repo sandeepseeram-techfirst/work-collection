09ceeeb1b862dc69a61bd175953cab93f96e5a61
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-03-02 18:43:19 +0530

    index.js added

---
+++ full-stack-web-development-nodejs-react/client-side/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+require('./style.css');
+
+import ReactDOM from 'react-dom';
+import App from './app.js';
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+);
