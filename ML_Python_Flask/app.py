74df765b295eeee3d0a546a92af2552b66532f09
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-03-10 02:23:39 +0530

    app.py file added

---
+++ ML_Python_Flask/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return 'Diamonds'
+
+@app.route("/result")
+def predict():
+    price = 1000
+    return str(price)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
