d5b0296be9083de91c6270d77528040b4ce18d27
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-03-13 04:41:23 +0530

    quant trading with python data loading

---
+++ quant_trading_with_python/sma_cma.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df =  pd.read_csv("https://raw.githubusercontent.com/dswh/python_fundamentals/master/images/apple_stock_eod_prices.csv",
+                parse_dates=True, header=0, index_col=0)
+
+# Isolate the adjusted closing prices into a series
+adj_close_px = df['Adj_Close']
+
+# Calculate the moving average - let's try it for a short period of # 40 days
+moving_avg = adj_close_px.rolling(window=40).mean()
+
+# Inspect the result
+print(moving_avg)
+
+# Short moving window rolling mean
+df['sma_40'] = adj_close_px.rolling(window=40).mean()
+
+# Long moving window rolling mean - 252 days
+df['sma_252'] = adj_close_px.rolling(window=252).mean()
+
+# Plot the adjusted closing price, the short and long windows of rolling means
+fig, ax = plt.subplots( nrows=1, ncols=1 )
+df[['Adj_Close', 'sma_40', 'sma_252']].plot(figsize=(12,8),ax=ax)
+fig.savefig('moving_avg.png')
+# Cumulative moving average
+df['cma'] = adj_close_px.expanding().mean()
+
+# Plot the adjusted closing price, the short and long windows of rolling means
+fig, ax = plt.subplots( nrows=1, ncols=1 )
+df[['Adj_Close', 'cma']].plot(figsize=(12,8),ax=ax)
+fig.savefig('cumulative_moving_avg.png')
