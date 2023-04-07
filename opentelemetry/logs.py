a46c6bc2bcf5bd6baea56206c1965dfa4ac05198
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-04-07 13:09:57 +0530

    opentelemetry code

---
+++ opentelemetry/logs.py
@@ -0,0 +1,37 @@
+import time
+
+from opentelemetry.sdk._logs import (
+
+    LogEmitterProvider,
+
+    LogRecord,
+
+    get_log_emitter_provider,
+
+    set_log_emitter_provider,
+
+)
+
+if __name__ == "__main__":
+
+    configure_log_emitter_provider()
+
+    log_emitter = get_log_emitter_provider().get_log_emitter(
+
+        "shopper",
+
+        "0.1.2",
+
+    )
+
+    log_emitter.emit(
+
+        LogRecord(
+
+            timestamp=time.time_ns(),
+
+            body="first log line",
+
+        )
+
+    )
