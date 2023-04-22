94cc5d3fdd6bbc8733db223fe31d69de5105780a
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-04-22 12:30:34 +0530

    cisco developer week - python

--- cisco_developer_week1/python/request.py
+++ cisco_developer_week1/python/request.py
@@ -15,3 +15,14 @@ def get_auth_token():
      token = resp.json()['Token']                                                          # Retrieve the Token
      print("Token Retrieved: {}".format(token))                                            # Print out the Token
      return token   
+
+def get_device_list():
+    """
+    Building out function to retrieve list of devices. Using requests.get to make a call to the network device Endpoint
+    """
+    token = get_auth_token()                                                               # Get Token
+    url = "https://{}/dna/intent/api/v1/network-device".format(DNAC_URL)                   # Network Device endpoint
+    hdr = {'x-auth-token': token, 'content-type' : 'application/json'}                     # Build header Info
+    resp = requests.get(url, headers=hdr)                                                  # Make the Get Request
+    device_list = resp.json()                                                              # Capture the data from the controller
+    print_device_list(device_list)    
