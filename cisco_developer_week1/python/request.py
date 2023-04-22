86b36a63a76a4f95757d26ad465087da3413e12c
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-04-22 12:31:07 +0530

    cisco developer week - python

--- cisco_developer_week1/python/request.py
+++ cisco_developer_week1/python/request.py
@@ -26,3 +26,21 @@ def get_device_list():
     resp = requests.get(url, headers=hdr)                                                  # Make the Get Request
     device_list = resp.json()                                                              # Capture the data from the controller
     print_device_list(device_list)    
+
+def print_device_list(device_json):
+     print("{0:42}{1:17}{2:12}{3:18}{4:12}{5:16}{6:15}".
+          format("hostname", "mgmt IP", "serial","platformId", "SW Version", "role", "Uptime"))
+     for device in device_json['response']:
+        uptime = "N/A" if device['upTime'] is None else device['upTime']
+        if device['serialNumber'] is not None and "," in device['serialNumber']:
+            serialPlatformList = zip(device['serialNumber'].split(","), device['platformId'].split(","))
+        else:
+            serialPlatformList = [(device['serialNumber'], device['platformId'])]
+        for (serialNumber, platformId) in serialPlatformList:
+            print("{0:42}{1:17}{2:12}{3:18}{4:12}{5:16}{6:15}".
+                  format(device['hostname'],
+                         device['managementIpAddress'],
+                         serialNumber,
+                         platformId,
+                         device['softwareVersion'],
+                         device['role'], uptime))
