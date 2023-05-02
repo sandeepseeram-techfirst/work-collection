d1a3fe52f343cca9369148deb613613864becff6
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-05-02 22:11:32 +0530

    predictnow python testing

--- quant_trading_with_python/predictnow.py
+++ quant_trading_with_python/predictnow.py
@@ -10,6 +10,13 @@ client = PredictNowClient(api_host,api_key)
 file_path = 'my_amazing_features.xlsx'
 labelname = 'Next_day_strategy_return'
 
-
-
+import os
  
+params = {'timeseries': 'yes', 'type': 'regression', 'feature_selection': 'none', 'analysis': 'none', 'boost': 'gbdt', 'testsize': '0.2', 'weights': 'no', 'eda': 'yes', 'prob_calib': 'no', 'mode': 'train'}
+
+# LET'S CREATE THE MODEL BY SENDING THE PARAMETERS TO PREDICTNOW.AI
+response = client.create_model(
+ username=username, # only letters, numbers, or underscores
+ model_name="test1",
+ params=params,
+)
