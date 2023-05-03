3937e175899d96687725a6104825d50c1f9e5900
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-05-03 22:12:07 +0530

    predictnow python testing

--- quant_trading_with_python/predictnow.py
+++ quant_trading_with_python/predictnow.py
@@ -20,3 +20,17 @@ response = client.create_model(
  model_name="test1",
  params=params,
 )
+ 
+# LET'S LOAD UP THE FILE TO PANDAS IN THE LOCAL ENVIRONMENT
+from pandas import read_csv # If you have the Excel file, replace read_csv with read_excel
+from pandas import read_excel
+df = read_excel(file_path, engine="openpyxl") # Same here
+df.name = "testdataframe" # Optional, but recommended
+response = client.train(
+ model_name="test1",
+ input_df=df,
+ label=labelname,
+ username=username,
+ email=email,
+ return_output=False,
+)
