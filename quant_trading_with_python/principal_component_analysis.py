43b949f10ac44596b9b41d555fee2229576d928f
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-05-05 22:24:22 +0530

    predictnow python testing

--- quant_trading_with_python/principal_component_analysis.py
+++ quant_trading_with_python/principal_component_analysis.py
@@ -13,3 +13,14 @@ from sklearn.preprocessing import StandardScaler
 from sklearn import linear_model
 from sklearn.linear_model import Ridge
 import time
+
+lookback=252 # training period for factor exposure
+numFactors=5
+topN=50 # for trading strategy, long stocks with topN exepcted 1-day returns
+df=pd.read_table('IJR_20080114.txt')
+df['Date']=df['Date'].astype('int')
+df.set_index('Date', inplace=True)
+df.sort_index(inplace=True)
+df.fillna(method='ffill', inplace=True)
+dailyret=df.pct_change() # note the rows of dailyret are the observations at different time periods
+positionsTable=np.zeros(df.shape)
