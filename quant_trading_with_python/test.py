2e426c30fd805e441af0ad4a2e31e84f07bffed1
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-05-05 22:20:10 +0530

    predictnow python testing

--- quant_trading_with_python/test.py
+++ quant_trading_with_python/test.py
@@ -3,3 +3,9 @@ for i in range(len(input_features)):
     #/ split y_pred[‘Date'] into actual date and parameters string
     date_params =input_features[i].split(' ') 
     params = date_params[1]
+   if i==0: 
+ #initializing max_index and its parameters value 
+    #E.g. (2.5, 60, 0.2)
+    params_cond_optimized = params 
+      y_pred_max = y_pred[i].values
+  
