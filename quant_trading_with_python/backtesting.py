54f1abef2a72c66f41605a49ea3d92a040a14e33
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-05-07 22:29:47 +0530

    backtesting python testing

--- quant_trading_with_python/backtesting.py
+++ quant_trading_with_python/backtesting.py
@@ -4,3 +4,7 @@ import pandas as pd
 df=pd.read_table('SPX_20071123.txt')
 df['Date']=df['Date'].round().astype('int')
 df['Date']=pd.to_datetime(df['Date'], format='%Y%m%d')
+df.set_index('Date', inplace=True)
+eomPrice=df.resample('M').last()[:-1] # End of month prices. Need to remove last date because it isn't really end of January.
+monthlyRet=eomPrice.pct_change(1, fill_method=None)
+positions=np.zeros(monthlyRet.shape)
