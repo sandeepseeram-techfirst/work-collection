daa925dde2485e9e28a85f8e68ba6a5968b0bb6f
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-05-07 22:29:30 +0530

    backtesting python testing

--- quant_trading_with_python/backtesting.py
+++ quant_trading_with_python/backtesting.py
@@ -1,3 +1,6 @@
 # Backtesting a Year-on-Year Seasonal Trending Strategy
 import numpy as np
 import pandas as pd
+df=pd.read_table('SPX_20071123.txt')
+df['Date']=df['Date'].round().astype('int')
+df['Date']=pd.to_datetime(df['Date'], format='%Y%m%d')
