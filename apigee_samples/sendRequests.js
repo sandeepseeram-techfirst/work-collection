e3bf71488a11e4e0e74b2c4dd875ffe3d2da0f81
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-05-22 11:15:20 +0530

    apigee send requests js added

---
+++ apigee_samples/sendRequests.js
@@ -0,0 +1,28 @@
+// search is a query parameter with a comma-separated list of topics to be searched
+var searchParam = context.getVariable("request.queryparam.search");
+if (searchParam === null || searchParam.length === 0) {
+    throw("search query parameter not found");
+}
+var search = searchParam.split(",");
+print("search=" + search);
+// call max 5 in parallel
+var maxCalls = search.length;
+if (maxCalls > 5) {
+    maxCalls = 5;
+}
+print("maxCalls=" + maxCalls);
+// URL to the Google Books API
+var url="https://www.googleapis.com/books/v1/volumes?country=US&q=subject:";
+var searchTerms = [];
+// for each search term, call the Google API
+for (var i=0; i < maxCalls; i++) {
+    var searchTerm = search[i];
+    print("" + i + ": " + searchTerm);
+    print("GET " + url + ""+searchTerm);
+    var req = httpClient.get(url+searchTerm);
+    // store the request in a session for later retrieval
+    searchTerms.push(searchTerm);
+    context.session["searchTerm:" + searchTerm] = req;
+}
+context.session["searchTerms"] = searchTerms;
+print("done sending the requests");
