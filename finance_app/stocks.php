240c7583c697a0b115943067b2480a074014da1c
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-05-28 21:19:14 +0530

    finance app

--- finance_app/stocks.php
+++ finance_app/stocks.php
@@ -5,5 +5,9 @@ $data = file_get_contents('php://input');
 
 $my_file = fopen("stocks.json", "w") or die("Unable to open file!");
 
+fwrite($my_file, $data);	
+fclose($my_file);
+//echo "File written successfully";
+
 
 ?>
