b3c38dedddf463809859b96db8481330901577cd
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-05-29 21:28:51 +0530

    finance app

--- finance_app/python-app/moneytransfers.py
+++ finance_app/python-app/moneytransfers.py
@@ -3,4 +3,5 @@ from accounts import Account
 class MoneyTransfer:
     @staticmethod
     def transfer_money(sender, receiver, amount):
-         
+        sender.withdraw(amount)
+        receiver.deposit(amount)
