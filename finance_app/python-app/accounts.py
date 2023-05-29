dd99e405b6497b91e721d7d8d43105843bf11edf
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-05-29 21:27:26 +0530

    finance app

--- finance_app/python-app/accounts.py
+++ finance_app/python-app/accounts.py
@@ -7,7 +7,11 @@ class Account:
     def deposit(self, amount):
         self.balance += amount
 
-
+        def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+        else:
+            raise ValueError("Not enough balance")
 
     def __str__(self):
         return f"Account {self.acc_id} owned by {self.owner} with balance {self.balance}"
