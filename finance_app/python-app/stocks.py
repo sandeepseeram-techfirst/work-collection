ec2ea2b917e4383798791850cf44a4bec03e7379
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-05-30 21:30:22 +0530

    finance app

--- finance_app/python-app/stocks.py
+++ finance_app/python-app/stocks.py
@@ -4,3 +4,8 @@ class Stock:
     def __init__(self, stock_id, price):
         self.stock_id = stock_id
         self.price = price
+
+    def generate_price(self):
+        self.price += random.uniform(-1, 1)
+
+
