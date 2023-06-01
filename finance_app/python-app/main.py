9869f69659f28217c4ed63339fa5f1f4d0538ad9
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-06-01 21:36:29 +0530

    finance app

--- finance_app/python-app/main.py
+++ finance_app/python-app/main.py
@@ -2,4 +2,10 @@ from accounts import Account
 from moneytransfers import MoneyTransfer
 from stocks import Stock
 
+def main():
+    acc1 = Account("1", 10000, "John")
+    acc2 = Account("2", 5000, "Jane")
+    print(acc1)
+    print(acc2)
 
+ 
