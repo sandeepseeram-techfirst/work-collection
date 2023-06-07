9bee342200adc313d00b4638fd3e18dab5a8ee24
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-06-07 15:42:13 +0530

    go bootcamp

--- go_bootcamp/functions/main.go
+++ go_bootcamp/functions/main.go
@@ -44,7 +44,7 @@ func main() {
     }
     f, err := div(a, b)
     if err != nil {
-        fmt.Println("dividing failed:", err)
+        fmt.Println("dividing failed:", err)``
     } else {
         fmt.Println(f)
     }
