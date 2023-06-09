63796599a7ec928c849b2e5c81720ac0eb6d0993
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-06-09 16:27:59 +0530

    go bootcamp

---
+++ go_bootcamp/greeting.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+
+const (
+	Pi    = 3.14
+	Truth = false
+	Big   = 1 << 62
+	Small = Big >> 61
+)
+
+func main() {
+	const Greeting = "ハローワールド" //declaring a constant
+	fmt.Println(Greeting)
+	fmt.Println(Pi)
+	fmt.Println(Truth)
+	fmt.Println(Big)
+}
