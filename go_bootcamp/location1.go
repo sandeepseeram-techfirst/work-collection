a43da730ab08ab0876432235d363d89c4a095ca5
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-06-12 16:55:05 +0530

    go bootcamp

---
+++ go_bootcamp/location1.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+
+func location(city string) (region, continent string) {
+	switch city {
+	case "Los Angeles", "LA", "Santa Monica":
+		region, continent = "California", "North America"
+	case "New York", "NYC":
+		region, continent = "New York", "North America"
+	default:
+		region, continent = "Unknown", "Unknown"
+	}
+	return //returning region and continent
+}
+
+func main() {
+	region, continent := location("Santa Monica")
+	fmt.Printf("Matt lives in %s, %s", region, continent)
+}
