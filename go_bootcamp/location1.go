5553ffd47e090d9b922b21e7974d24de8f69f8b5
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-06-13 17:07:45 +0530

    go bootcamp

--- go_bootcamp/location1.go
+++ go_bootcamp/location1.go
@@ -5,7 +5,7 @@ func location(city string) (region, continent string) {
 	switch city { 
 	case "Los Angeles", "LA", "Santa Monica":
 		region, continent = "California", "North America"
-	case "New York", "NYC":
+	case "New York", "NYC": 
 		region, continent = "New York", "North America"
 	default:
 		region, continent = "Unknown", "Unknown"
