bb3b60c1893b9cff300e378383f6d6492b59e6e0
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-06-15 22:38:13 +0530

    go bootcamp

---
+++ go_bootcamp/break.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	pow := make([]int, 10)
+	for i := range pow {
+		pow[i] = 1 << uint(i)
+		if pow[i] >= 16 {
+			break //stop iterating over pow when it reaches 16
+		}
+	}
+	fmt.Println(pow)
+	// [1 2 4 8 16 0 0 0 0 0]
+} 
