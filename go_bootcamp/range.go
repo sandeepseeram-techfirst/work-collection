d734f910586f1d158f397bee9484118bdd370f44
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-06-16 22:41:26 +0530

    go bootcamp

--- go_bootcamp/range.go
+++ go_bootcamp/range.go
@@ -4,7 +4,7 @@ import "fmt"
 
 var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
 
-func main() {
+func main() { 
     for i, v := range pow { //loops over the length of pow
         fmt.Printf("2**%d = %d\n", i, v)
     }
