6b6063f549cea265e3a03cbf8e1f1ef9c21ae314
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-06-16 22:41:32 +0530

    go bootcamp

--- go_bootcamp/range1.go
+++ go_bootcamp/range1.go
@@ -7,7 +7,7 @@ func main() {
     for i := range pow {
         pow[i] = 1 << uint(i)
     }
-    for _, value := range pow {
+    for _, value := range pow { 
         fmt.Printf("%d\n", value)
     }
 }  
