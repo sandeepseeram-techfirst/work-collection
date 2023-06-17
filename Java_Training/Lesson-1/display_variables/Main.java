51e558ad739247abf1f095a9e7b9f2a45b328533
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-06-17 13:32:53 +0530

    java -intro - training

---
+++ Java_Training/Lesson-1/display_variables/Main.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        String name = "John";
+        int myNum = 15;
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+        System.out.println(name);
+        System.out.println(myNum);
+        String fullname = "John";
+        System.out.println("Hello " + fullname);
+    }
+}
