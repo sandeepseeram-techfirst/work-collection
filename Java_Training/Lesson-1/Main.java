6662dbd763e6b30ed98453bbc51fb083ff4aeb78
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-06-17 13:10:54 +0530

    java -intro - training

---
+++ Java_Training/Lesson-1/Main.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+    }
+}
