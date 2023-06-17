e8f963fb525803932cf2d8e5d141df48829ce7cf
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-06-17 13:32:36 +0530

    java -intro - training

---
+++ Java_Training/Lesson-1/display_variables/main1.java
@@ -0,0 +1,8 @@
+public class main1 {
+  public static void main(String[] args) {
+    String firstName = "John ";
+    String lastName = "Doe";
+    String fullName = firstName + lastName;
+    System.out.println(fullName);  
+  }
+}
