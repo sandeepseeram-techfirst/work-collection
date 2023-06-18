e7541cc05d2ac2b7a51e89a18a98cec44b5e15c1
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-06-18 14:01:52 +0530

    java -intro - training

---
+++ Java_Training/Lesson-1/StringConcatenation/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String args[]) {
+    String firstName = "John";
+    String lastName = "Doe";
+    System.out.println(firstName + " " + lastName);
+  }
+}
