d9ae78fd13551cb61a5a6769a5279ba932d99076
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-06-18 13:48:53 +0530

    java -intro - training

---
+++ Java_Training/Lesson-1/Type_Casting/Narrowing/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    double myDouble = 9.78d;
+    int myInt = (int) myDouble; // Manual casting: double to int
+
+    System.out.println(myDouble);   // Outputs 9.78
+    System.out.println(myInt);      // Outputs 9
+  }
+}
