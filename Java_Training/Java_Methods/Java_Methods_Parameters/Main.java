8e5a2ba3953cd8cb603083ab1a51202abf7391a3
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-06-20 17:22:44 +0530

    java -intro - training

---
+++ Java_Training/Java_Methods/Java_Methods_Parameters/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  static void myMethod(String fname) {
+    System.out.println(fname + " Seeram");
+  }
+
+  public static void main(String[] args) {
+    myMethod("Ramesh Kumar");
+    myMethod("Amit Kumar");
+    myMethod("Sandeep Kumar");
+  }
+}
