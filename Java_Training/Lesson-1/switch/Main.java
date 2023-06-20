b47a8d40d28bb5195dc050622e374fa23ddb43e0
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-06-20 16:25:00 +0530

    java -intro - training

--- Java_Training/Lesson-1/switch/Main.java
+++ Java_Training/Lesson-1/switch/Main.java
@@ -2,7 +2,7 @@ public class Main {
   public static void main(String[] args) {
     int day = 6;
     switch (day) {
-      case 1:
+      case 1: 
         System.out.println("Monday");
         break;
       case 2:
