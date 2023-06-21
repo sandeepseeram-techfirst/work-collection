9461f1b3659be3d010fd95ce3425134ff689b4ed
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-06-21 18:08:56 +0530

    java -intro - training

---
+++ Java_Training/Encapsulation/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+   private String name;
+
+   // Getter
+   public String getName() {
+     return name;
+   }
+
+   // Setter
+   public void setName(String newName) {
+     this.name = newName;
+   }
+}
