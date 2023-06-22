ff87f759b1199028036d71be88d0307b16ea0acb
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-06-22 18:34:08 +0530

    java -intro - training

--- Java_Training/java_dates/Main.java
+++
@@ -1,8 +0,0 @@
-import java.time.LocalDate; // import the LocalDate class
-
-public class Main {
-  public static void main(String[] args) {
-    LocalDate myObj = LocalDate.now(); // Create a date object
-    System.out.println(myObj); // Display the current date
-  }
-}
