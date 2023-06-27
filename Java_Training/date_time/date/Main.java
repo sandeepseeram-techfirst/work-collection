49c2bb89b905bd02dff6ac97122cbc52a73dfb07
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-06-28 00:27:56 +0530

    java -intro - training

--- Java_Training/date_time/date/Main.java
+++ Java_Training/date_time/date/Main.java
@@ -4,5 +4,5 @@ public class Main {
   public static void main(String[] args) {
     LocalDate myObj = LocalDate.now(); // Create a date object
     System.out.println(myObj); // Display the current date
-  }
+  } 
 }
