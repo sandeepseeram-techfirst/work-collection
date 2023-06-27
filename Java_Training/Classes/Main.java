837b47cb8a7eff9417b2182580efd8e84f82f6bc
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-06-27 22:51:37 +0530

    java -intro - training

--- Java_Training/Classes/Main.java
+++ Java_Training/Classes/Main.java
@@ -6,7 +6,7 @@ class Main {
     String userName;
     
     // Enter username and press Enter
-    System.out.println("Enter username"); 
+    System.out.println("Enter username");  
     userName = myObj.nextLine();   
        
     System.out.println("Username is: " + userName);        
