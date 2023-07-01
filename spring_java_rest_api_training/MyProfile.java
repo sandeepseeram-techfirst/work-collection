f75988ef18c484b433bbba06ce186a2868746cb3
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-07-01 14:20:23 +0530

    java training

---
+++ spring_java_rest_api_training/MyProfile.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+		String name = "Laura";
+    int age = 25;
+    double desiredSalary = 80000.00;
+    char gender = 'f';
+    boolean lookingForJob = true;
+	}
+}
