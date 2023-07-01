02f8ba0aa7535654661057882a97734d65395b2c
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-07-01 15:32:39 +0530

    java into

---
+++ spring_java_rest_api_training/BakeSale.java
@@ -0,0 +1,13 @@
+public class BakeSale {
+    public static void main(String[] args) {
+        int numCookies = 17;
+
+        // Customer buys 3 cookies
+        numCookies -= 3;
+
+        // Another customer buys half of the remaining cookies
+        numCookies /= 2;
+
+        System.out.println(numCookies);
+    }
+}
