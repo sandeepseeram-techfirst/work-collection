bbe7c5e67bcffc4044c2a7794b4d24b7c9175e9f
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-07-01 14:07:02 +0530

    java basic classes

---
+++ spring_java_rest_api_training/CountComment.java
@@ -0,0 +1,10 @@
+public class CountComment {
+    public static void main(String[] args) {
+        // This is where you will define your variable
+        
+        // This is where you will print your variable
+        
+        int numComments = 6;
+        System.out.println("Number of comments: " + numComments);
+    }
+}
---
+++ spring_java_rest_api_training/MarketShare.java
@@ -0,0 +1,6 @@
+public class MarketShare {
+	public static void main(String[] args) {
+    double androidShare = 81.7;
+    System.out.println(androidShare);
+	}
+}
