ddc3d2593a68c18ce29e48d5f2e2dec9a6cbe093
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-07-08 12:06:26 +0530

    controller java spring

--- spring-framework/SuperHeroController.java
+++ spring-framework/SuperHeroController.java
@@ -1,11 +1,18 @@
 package com.codecademy.ccapplication;
+
 import java.util.List;
 import java.lang.Iterable;
 import java.util.Date;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
+@RequestMapping("/superHeroes")
 public class SuperHeroController {
 
   private final SuperHeroRepository superHeroRepository;
@@ -16,10 +23,22 @@ public class SuperHeroController {
     this.superReportRepository = superReportRepository;
   }
 
-  @RequestMapping("/superHeroes")
+@GetMapping()
 	public Iterable<SuperHero> getSuperHeros() {
     Iterable<SuperHero> superHeroes = superHeroRepository.findAll();
     return superHeroes;
 	}
 
+@GetMapping("/heroReport")
+public Iterable<SuperReport> getHeroReport() {
+  Iterable<SuperReport> superReport = superReportRepository.findAll();
+  return superReport;
+}
+
+@PostMapping("/addNew")
+public String createNewSuperHero(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String superPower) {
+  SuperHero newSuperHero = new SuperHero(firstName, lastName, superPower);
+  superHeroRepository.save(newSuperHero);
+  return "New Super Hero successfully added!";
+}
 }
