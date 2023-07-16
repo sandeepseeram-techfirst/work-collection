75c882a310dfd0b94d2f4d63b1933a1b878c0e08
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-07-16 16:01:48 +0530

    python django

--- django_python/views.py
+++ django_python/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
 
-def members(request):
+def members(request): 
   template = loader.get_template('myfirst.html')
   return HttpResponse(template.render()) 
