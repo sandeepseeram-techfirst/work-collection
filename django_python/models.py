4733ff41f30c3489339721d382c0d0fd9f69b77a
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-07-18 16:06:36 +0530

    python django

--- django_python/models.py
+++ django_python/models.py
@@ -2,4 +2,4 @@ from django.db import models
  
 class Member(models.Model):
   firstname = models.CharField(max_length=255)
-  lastname = models.CharField(max_length=255) 
+  lastname = models.CharField(max_length=255)  
