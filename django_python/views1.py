0551857eac14fa73deffe1d88d4f863d2dee0dde
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-07-20 16:38:28 +0530

    python django

---
+++ django_python/views1.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+def testing(request):
+  mydata = Member.objects.all()
+  template = loader.get_template('template.html')
+  context = {
+    'mymembers': mydata,
+  }
+  return HttpResponse(template.render(context, request))
