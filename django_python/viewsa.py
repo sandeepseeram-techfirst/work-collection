9b371ae5332df145163d3ca720358ea1a034d9cd
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-07-20 16:11:00 +0530

    python django

---
+++ django_python/viewsa.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.template import loader
+
+def testing(request):
+  template = loader.get_template('template.html')
+  context = {
+    'firstname': 'Linus',
+  }
+  return HttpResponse(template.render(context, request))     
