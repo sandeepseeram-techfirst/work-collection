b0fe9c5968b8b57eafa68f338ccd402a4e1525c7
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-07-23 23:48:53 +0530

    python django

--- django_python/django_cryptocurrencies/urls.py
+++ django_python/django_cryptocurrencies/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
 from . import views
 
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('websocket/', views.ws_connect, name='ws_connect'),
+    path('websocket/<str:symbol>/', views.ws_price, name='ws_price'),
+]
