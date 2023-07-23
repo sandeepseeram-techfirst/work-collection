619a005b7431d787a948283e71f270a4ca5a02e7
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-07-23 23:43:48 +0530

    python django

--- django_python/django_cryptocurrencies/models.py
+++ django_python/django_cryptocurrencies/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Cryptocurrency(models.Model):
     symbol = models.CharField(max_length=10)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50) 
     price = models.FloatField()
     updated_at = models.DateTimeField(auto_now=True)
