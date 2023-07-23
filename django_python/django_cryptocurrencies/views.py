85b0b6e83a72862ca27b6c6b3eb5dac9d516e14e
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-07-23 23:45:56 +0530

    python django

--- django_python/django_cryptocurrencies/views.py
+++ django_python/django_cryptocurrencies/views.py
@@ -3,3 +3,25 @@ from .models import Cryptocurrency
 
 sio = socketIO.Server()
 
+@sio.event
+def connect():
+    print('Client connected!')
+
+@sio.event
+def disconnect():
+    print('Client disconnected!')
+
+@sio.event
+def message(message):
+    try:
+        data = json.loads(message)
+        if data['type'] == 'price':
+            symbol = data['symbol']
+            price = data['price']
+            cryptocurrency = Cryptocurrency.objects.get(symbol=symbol)
+            cryptocurrency.price = price
+            cryptocurrency.save()
+            sio.emit('price', {'symbol': symbol, 'price': price})
+    except Exception as e:
+        print(e)
+
