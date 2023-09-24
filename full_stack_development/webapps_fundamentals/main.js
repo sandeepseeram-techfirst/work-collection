2d7f40605cb8807aaebdaa3bfb731382d75417a6
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-09-24 18:42:10 +0530

    angular js

--- full_stack_development/webapps_fundamentals/main.js
+++ full_stack_development/webapps_fundamentals/main.js
@@ -3,7 +3,7 @@ var xhttp = new XMLHttpRequest()
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     const data = JSON.parse(this.responseText)
-    console.log(data)
+    console.log(data) 
 
     var ul = document.createElement('ul')
     ul.setAttribute('class', 'notes')
